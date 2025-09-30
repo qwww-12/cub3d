36411c325bd9d3809b84037e79b3671984c69d22
Mohamed Ayoub Barhoun <mbarhoun@c1r3p5.1337.ma>
2025-09-30 18:08:44 +0100

    update

--- parsing/cub3d_map.c
+++ parsing/cub3d_map.c
@@ -6,7 +6,7 @@
 /*   By: mbarhoun <mbarhoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 17:38:58 by mbarhoun          #+#    #+#             */
-/*   Updated: 2025/09/30 18:03:57 by mbarhoun         ###   ########.fr       */
+/*   Updated: 2025/09/30 18:08:19 by mbarhoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,12 @@ static bool	check_map_content(t_config *config, int ln)
 	int	p_block;
 
 	p_block = 1;
-	write(1, "ll\n", 3);
 	if (!first_line_mp(config, 0, 0))
 		return (0);
-	write(1, "ll\n", 3);
 	if (!check_map_walls(config, &p_block, ln - 2, 0))
 		return (0);
-	write(1, "ll\n", 3);
 	if (!last_line_mp(config, p_block, 0))
 		return (0);
-	write(1, "ll\n", 3);
 	return (1);
 }
 
