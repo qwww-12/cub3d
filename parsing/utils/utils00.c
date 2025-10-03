68dd0bace727790380fd4a3098d95d4dd301bc59
Mohamed Ayoub Barhoun <mbarhoun@c1r2p2.1337.ma>
2025-10-03 20:14:00 +0100

    update

--- parsing/utils/utils00.c
+++ parsing/utils/utils00.c
@@ -6,7 +6,7 @@
 /*   By: mbarhoun <mbarhoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 16:57:30 by mbarhoun          #+#    #+#             */
-/*   Updated: 2025/09/27 17:46:43 by mbarhoun         ###   ########.fr       */
+/*   Updated: 2025/10/03 20:13:21 by mbarhoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	open_fd(char *file, int fd)
 	fd = open(file, O_RDONLY);
 	if (fd < 0)
 	{
-		perror("Problem in Opening file\n");
+		p_error("Problem in Opening file\n");
 		exit(1);
 	}
 	return (fd);
